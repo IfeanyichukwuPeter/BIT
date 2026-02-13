24ab4a9ec56510112ebb27e76a987ffb0a73b307
Ifeanyichukwu Peter <mbaifeanyichukwu70@gmail.com>
2026-02-13 14:34:35 +0100

    improved changes

--- bit.js
+++ bit.js
@@ -11,22 +11,36 @@ fetch("components/footer.html")
     document.getElementById("footer").innerHTML = data;
   });
 
+
 function initMenu() {
   const menu = document.getElementById("menu");
-  const menuBtn = document.getElementById("menu-btn");
-  const menuIcon = document.getElementById("burger");
+  const oldBtn = document.getElementById("menu-btn");
 
-  let menuOpen = false;
+  if (!menu || !oldBtn) return;
+
+  const menuBtn = oldBtn.cloneNode(true);
+  oldBtn.parentNode.replaceChild(menuBtn, oldBtn);
+
+  const menuIcon = menuBtn.querySelector("#burger");
 
   menuBtn.addEventListener("click", () => {
-    menuOpen = !menuOpen;
-    if (menuOpen) {
-      menu.style.display = "flex";
-      menuIcon.src = "/bitimg/icon-close.svg";
-    } else {
-      menu.style.display = "none";
-      menuIcon.src = "/bitimg/icon-hamburger.svg";
-      `mk7`;
-    }
+    console.log("clicked");
+
+    menu.classList.toggle("active");
+    const isOpen = menu.classList.contains("active");
+
+   menuIcon.style.opacity = "0";
+
+setTimeout(() => {
+  menuIcon.src = isOpen
+    ? "/bitimg/icon-close.svg"
+    : "/bitimg/icon-hamburger.svg";
+
+  menuIcon.style.opacity = "1";
+}, 150);
+menuIcon.style.transform = isOpen
+  ? "rotate(90deg)"
+  : "rotate(0deg)";
+
   });
 }
